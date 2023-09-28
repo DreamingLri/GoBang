63cdf5b2f2a207efc6ac587494d2f35b258bcc07
DreamingLri <3507415455@qq.com>
2023-09-28 20:16:35 +0800

    Update demo1.cpp

--- demo1.cpp
+++ demo1.cpp
@@ -4,7 +4,10 @@ int main(){
     cout << "Hello World" << endl;
 <<<<<<< HEAD
     cout << "sss" << endl;
+<<<<<<< HEAD
     //11111
 =======
 >>>>>>> parent of b8b3b13 (Update demo1.cpp)
+=======
+>>>>>>> parent of ed285d7 (Update demo1.cpp)
 }
