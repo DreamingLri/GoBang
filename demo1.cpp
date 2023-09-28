cc5799e36b6d3b23948b7c691fe0dbf9dd0c843d
DreamingLri <3507415455@qq.com>
2023-09-28 20:17:41 +0800

    Update demo1.cpp

--- demo1.cpp
+++ demo1.cpp
@@ -2,12 +2,8 @@
 using namespace std;
 int main(){
     cout << "Hello World" << endl;
-<<<<<<< HEAD
+
     cout << "sss" << endl;
-<<<<<<< HEAD
+
     //11111
-=======
->>>>>>> parent of b8b3b13 (Update demo1.cpp)
-=======
->>>>>>> parent of ed285d7 (Update demo1.cpp)
 }
